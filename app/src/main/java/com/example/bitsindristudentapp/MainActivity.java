5e356f4dccc560b17ea2739944ed9b0a68509d4e
lonewolf2299 <priyanshukatyayni229@gmail.com>
2023-07-01 11:07:35 +0530

    splash screen and home fragment

--- app/src/main/java/com/example/bitsindristudentapp/MainActivity.java
+++ app/src/main/java/com/example/bitsindristudentapp/MainActivity.java
@@ -1,14 +1,104 @@
 package com.example.bitsindristudentapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.frame_layout);
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+       /* switch (item.getItemId()) {
+
+            case R.id.navigation_video:
+                Toast.makeText(this, "Video lectures", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_rate:
+                Toast.makeText(this, "6000/- ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_ebook:
+                Toast.makeText(this, "Ebooks", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_theme:
+                Toast.makeText(this, "change themes", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_website:
+                Toast.makeText(this, "website here", Toast.LENGTH_SHORT).show();
+                break;
+        }*/
+
+
+
+
+        if (item.getItemId()==R.id.navigation_video){
+            Toast.makeText(this, "Video lectures", Toast.LENGTH_SHORT).show();
+
+        } else if (item.getItemId()==R.id.navigation_rate) {
+            Toast.makeText(this, "6000/- ", Toast.LENGTH_SHORT).show();
+
+        }else if (item.getItemId()==R.id.navigation_ebook) {
+            Toast.makeText(this, "ebook", Toast.LENGTH_SHORT).show();
+
+        }else if (item.getItemId()==R.id.navigation_theme) {
+            Toast.makeText(this, "themes", Toast.LENGTH_SHORT).show();
+
+        }else if (item.getItemId()==R.id.navigation_website) {
+            Toast.makeText(this, "website", Toast.LENGTH_SHORT).show();
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/bitsindristudentapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bitsindristudentapp/ui/home/HomeFragment.java
@@ -8,17 +8,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.AnimationTypes;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.bitsindristudentapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
-
+    private ImageSlider imageSlider;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        imageSlider = view.findViewById(R.id.slider);
+        imageSlider.setSlideAnimation(AnimationTypes.DEPTH_SLIDE);
+
+        List<SlideModel> imageList = new ArrayList<>();
+
+        imageList.add(new SlideModel("https://bit.ly/2YoJ77H", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/2BteuF2", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72", ScaleTypes.CENTER_CROP));
+
+
+
+
+
+        imageSlider.setImageList(imageList);
+        return view;
     }
 }
